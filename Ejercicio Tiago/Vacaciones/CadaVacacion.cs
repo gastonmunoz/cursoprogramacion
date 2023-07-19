d38c7797260bbd721fba69be2de7440454b0edca
sproko91 <s.prokopiec@gmail.com>
2023-07-19 19:43:54 -0300

    arreglos Título de ventanas Manejo de ventana (maximizar, etc). Agregar nueva -> Agregar o nueva Agregar, mouse se queda pensando Títulos de mensajes Eliminar con uno existente, si elijo la fila vacia, no hace nada

---
+++ Ejercicio Tiago/Vacaciones/CadaVacacion.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Vacaciones
+{
+        public class CadaVacacion
+        {
+            public int id { get; set; }
+            public string nombre { get; set; }
+            public string descripcion { get; set; }
+            public string empleada { get; set; }
+            public DateTime fechaDesde { get; set; }
+            public DateTime fechaHasta { get;set; }   
+        }
+}
--- Ejercicio Tiago/Vacaciones/Form1.cs
+++ Ejercicio Tiago/Vacaciones/Form1.cs
@@ -1,19 +1,23 @@
+using System.Text.Json;
+using System.Windows.Forms;
+
 namespace Vacaciones
 {
     public partial class Form1 : Form
     {
         private int Id = 1;
-
+        private string carpetaActual = Path.GetDirectoryName(Application.ExecutablePath);
+        private string nombreArchivoData;
+        public List<CadaVacacion> lista = new List<CadaVacacion>();
         public Form1()
         {
+            nombreArchivoData = $"{carpetaActual}\\data.json";
             InitializeComponent();
         }
-
         private void buttonAgregar_Click(object sender, EventArgs e)
         {
             mostrarFormEdicion(true);
         }
-
         private void buttonModificar_Click(object sender, EventArgs e)
         {
             if (dataGridVacaciones.CurrentCell.RowIndex >= 0 &&
@@ -21,28 +25,73 @@ namespace Vacaciones
             {
                 mostrarFormEdicion(false);
             }
-        }
-
-        private void buttonEliminar_Click(object sender, EventArgs e)
-        {
-            if (dataGridVacaciones.CurrentCell.RowIndex >= 0 &&
-                dataGridVacaciones.CurrentCell.RowIndex < dataGridVacaciones.Rows.Count - 1)
+            else
             {
-                dataGridVacaciones.Rows.RemoveAt(dataGridVacaciones.CurrentRow.Index);
+                mostrarError("No hay filas para modificar");
             }
         }
-
+        private void buttonEliminar_Click(object sender, EventArgs e)
+        {       // ELIMINA LA FILA SELECCIONANDO LA MISMA O ESTANDO POSICIONADO EN UNA CELDA CUALQUIERA
+            // Crea una lista para agregar rows que se deletean
+            List<DataGridViewRow> rowsAEliminar = new List<DataGridViewRow>();
+            // Recorre todas las filas del DataGridView utilizando foreach
+            foreach (DataGridViewRow row in dataGridVacaciones.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    // Recorre todas las celdas de la fila actual
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        // Comprueba si la celda actual está seleccionada
+                        if (cell.Selected)
+                        {
+                            // Agrega la fila a la lista de filas a eliminar
+                            rowsAEliminar.Add(row);
+                            break; // Sale del bucle foreach de filas después de agregar la fila
+                        }
+                    }
+                }
+                else if (dataGridVacaciones.CurrentCell.RowIndex == dataGridVacaciones.Rows.Count - 1)
+                {
+                    mostrarError("No se puede eliminar filas vacias");
+                }
+            }
+            foreach (DataGridViewRow rowAEliminar in rowsAEliminar)
+            {
+                dataGridVacaciones.Rows.Remove(rowAEliminar);
+            }
+        }
         private void habilitarCampos(bool editado)
         {
             buttonEliminar.Enabled = editado;
             buttonModificar.Enabled = editado;
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            habilitarCampos(false);
-        }
+            if (File.Exists(nombreArchivoData))
+            {
+                string json = File.ReadAllText(nombreArchivoData);
+                try
+                {
+                    lista = JsonSerializer.Deserialize<List<CadaVacacion>>(json);
 
+                    foreach (var cadavacacion in lista)
+                    {
+                        dataGridVacaciones.Rows.Add(cadavacacion.id, cadavacacion.nombre, cadavacacion.descripcion, cadavacacion.empleada, cadavacacion.fechaDesde.ToShortDateString(), cadavacacion.fechaHasta.ToShortDateString());
+                        Id = cadavacacion.id + 1;
+                    }
+                    habilitarCampos(true);
+                }
+                catch (Exception)
+                {
+                    mostrarError("Falló la conversión desde JSON");
+                }
+            }
+            else
+            {
+                habilitarCampos(false);
+            }
+        }
         private void mostrarFormEdicion(bool esAlta) 
         {
             FormEdicion formEdicion = new FormEdicion();
@@ -59,7 +108,7 @@ namespace Vacaciones
             {
                 if (esAlta)
                 {
-                    dataGridVacaciones.Rows.Add(Id, formEdicion.nombre, formEdicion.descripcion, formEdicion.empleada, formEdicion.fechaDesde.ToShortDateString(), formEdicion.fechaHasta.ToShortDateString()) ;
+                    dataGridVacaciones.Rows.Add(Id, formEdicion.nombre, formEdicion.descripcion, formEdicion.empleada, formEdicion.fechaDesde.ToShortDateString(), formEdicion.fechaHasta.ToShortDateString());
                     Id++;
                     habilitarCampos(true);
                 }
@@ -73,10 +122,39 @@ namespace Vacaciones
                 }
             }
         }
-
-        private void dataGridVacaciones_SelectionChanged(object sender, EventArgs e)
+        private void buttonExportar_Click(object sender, EventArgs e)
         {
-            buttonModificar.Enabled = (dataGridVacaciones.SelectedRows.Count == 1) && (dataGridVacaciones.CurrentRow.Index != dataGridVacaciones.Rows.Count - 1);
+            if (dataGridVacaciones.Rows.Count > 1 || (dataGridVacaciones.Rows.Count == 1 && MessageBox.Show("Esta seguro de guardar vacio?", "Cuidado!", MessageBoxButtons.OKCancel) == DialogResult.OK))
+            {
+                lista = new List<CadaVacacion>();
+                //Recorrer toda la grilla, campo por campo, y guardarlo en un JSON.
+                int fila = dataGridVacaciones.Rows.GetFirstRow(DataGridViewElementStates.None);
+                while (fila < dataGridVacaciones.Rows.Count - 1)
+                {
+                    var row = dataGridVacaciones.Rows[fila];
+                    CadaVacacion cadavacacion = new CadaVacacion();
+                    cadavacacion.id = Int32.Parse(row.Cells[0].Value.ToString());
+                    cadavacacion.nombre = row.Cells[1].Value.ToString();
+                    cadavacacion.descripcion = row.Cells[2].Value.ToString();
+                    cadavacacion.empleada = row.Cells[3].Value.ToString();
+                    cadavacacion.fechaDesde = DateTime.Parse(row.Cells[4].Value.ToString());
+                    cadavacacion.fechaHasta = DateTime.Parse(row.Cells[5].Value.ToString());
+                    lista.Add(cadavacacion);
+                    fila = dataGridVacaciones.Rows.GetNextRow(fila, DataGridViewElementStates.None);
+                }
+                //guardarlo en json
+                string json = JsonSerializer.Serialize(lista);
+                File.WriteAllText(nombreArchivoData, json);
+                MessageBox.Show("Stock guardado correctamente", "Informacion");
+            }
+        }
+        private void dataGridVacaciones_SelectionChanged(object sender, EventArgs e) 
+        { 
+            buttonModificar.Enabled = (dataGridVacaciones.SelectedRows.Count == 1) && (dataGridVacaciones.CurrentRow !=null) && (dataGridVacaciones.CurrentRow.Index != dataGridVacaciones.Rows.Count - 1);
+        }
+        private void mostrarError(string mensaje) 
+        {
+            MessageBox.Show(mensaje, "Error");
         }
     }
 }
